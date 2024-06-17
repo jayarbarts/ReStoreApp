417cfabf325460316f21a69b3fb1851ee590f00d
Filemon Bartolo <fcbartolojr26@gmail.com>
2024-06-17 21:04:16 +0800

    Added API controllers for the ReStoreApp

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+using API.Data;
+using API.Entities;
+using API.Extensions;
+using API.RequestHelpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams)
+        {
+            var query = _context.Products
+                .Sort(productParams.OrderBy)
+                .Search(productParams.SearchTerm)
+                .Filter(productParams.Brands, productParams.Types)
+                .AsQueryable();
+
+            var products = await PagedList<Product>.ToPagedList(query,
+                productParams.PageNumber, productParams.PageSize);
+            
+            Response.AddPaginationHeader(products.MetaData);
+
+            return products;
+
+            // return await query.ToListAsync();
+            // return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")] // api/products/3
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            return product;
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+            return Ok(new{brands, types});
+        }
+    }
+}
